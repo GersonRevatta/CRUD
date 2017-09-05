01be17e926cd00a7b2f5b154665a47bbfdb1a37f
gerson <gersonrevatta@gmail.com>
2017-09-05 16:05:40 -0500

    Migrations database

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 20170905164203) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comentarios", force: :cascade do |t|
     t.text "mensaje"
-    t.integer "tarea_id"
+    t.bigint "tarea_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["tarea_id"], name: "index_comentarios_on_tarea_id"
@@ -29,4 +32,22 @@ ActiveRecord::Schema.define(version: 20170905164203) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "comentarios", "tareas"
 end

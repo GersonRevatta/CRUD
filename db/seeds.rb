017d0ca95dac6e1b7922f0eef85a3ac8edf8e88d
gerson <gersonrevatta@gmail.com>
2017-09-12 11:37:35 -0500

    Adding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Tarea.create([
+  {
+  titulo: "Play soocer",
+  descripcion: "Play soocer",
+  prioridad: 1,
+  activo: true
+  },
+  {
+  titulo: "Play in the beach",
+  descripcion: "Play soocer",
+  prioridad: 3,
+  activo: true
+  },
+  {
+  titulo: "Play soocer",
+  descripcion: "Play soocer",
+  prioridad: 5,
+  activo: true
+  },
+  {
+  titulo: "Play  in  stadium",
+  descripcion: "Play soocer",
+  prioridad: 2,
+  activo: true
+  },
+  {
+  titulo: "Play football",
+  descripcion: "Play soocer",
+  prioridad: 1,
+  activo: true
+  },
+  {
+  titulo: "Play basketball",
+  descripcion: "Play football soocer",
+  prioridad: 5,
+  activo: true
+  },
+  {
+  titulo: "Play soocer and basketball",
+  descripcion: "Play soocer",
+  prioridad: 2,
+  activo: true
+  }
+  ])
+
+
